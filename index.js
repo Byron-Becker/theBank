d7213fe282c735dfcbb68c683c7169f59bc22f83
bbeck9123 <byron.ptgo@gmail.com>
2023-10-24 20:22:40 -0700

    theBAnk

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // the z index deals with how different elements of html will overlay each other when styled by css.  This is important with elements that will overlap others like drop down navigation bars.  The z index determines which item will be in front of the other.  Th element has to be positioned with fixed, absolute, sticky, relative, the parent will overlap base on the height, and children take on the z index of the closest parent.
+
+// floats purpose are to position elements within a container.  By floating an image for example you can position it within the container horizontally, saying left or right and the other content will flow around it.  This has been mostly outdated by flexbox and grid, but there are still a few examples of where it can be used.
+// It's important to be mindful of what happen
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
