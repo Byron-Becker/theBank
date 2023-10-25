18c99b9f63f27440a99600467b8bc73ef6364364
bbeck9123 <byron.ptgo@gmail.com>
2023-10-24 20:19:22 -0700

    theBAnk

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-testing
+// the z index deals with how different elements of html will overlay each other when styled by css.  This is important with elements that will overlap others like drop down navigation bars.  The z index determines which item will be in front of the other.  Th element has to be positioned with fixed, absolute, sticky, relative, the parent will overlap base on the height, and children take on the z index of the closest parent.
